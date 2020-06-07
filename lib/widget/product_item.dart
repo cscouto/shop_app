3833c06e3a3c185d0434ba5186a967efa3694f96
Tiago Do Couto <cs.couto@outlook.com>
2020-06-07 12:51:59 -0500

    initial

---
+++ lib/widget/product_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/model/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product _product;
+  final Key key;
+
+  ProductItem(this.key, this._product) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          _product.imageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          title: Text(
+            _product.title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
