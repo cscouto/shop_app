c25ecba9a729c04a531816d36d99c9477503b2e0
Tiago Do Couto <cs.couto@outlook.com>
2020-06-24 16:30:12 -0500

    implemented detail page

--- lib/widget/product_detail_page.dart
+++ lib/widget/product_detail_page.dart
@@ -12,6 +12,38 @@ class ProductDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(_product.title),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                _product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${_product.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                _product.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
