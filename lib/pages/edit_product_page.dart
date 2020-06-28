555a9fdbae88f24cceb9fa52318fccbdabf1cc59
Tiago Do Couto <cs.couto@outlook.com>
2020-06-27 20:52:21 -0500

    submit and validation

--- lib/pages/edit_product_page.dart
+++ lib/pages/edit_product_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/model/product.dart';
 
 class EditProductPage extends StatefulWidget {
   static const routeName = '/edit-product';
@@ -9,12 +10,31 @@ class EditProductPage extends StatefulWidget {
 class _EditProductPageState extends State<EditProductPage> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageController = TextEditingController();
+  final _form = GlobalKey<FormState>();
+
+  var _product = Product(
+    id: DateTime.now().toString(),
+    title: "",
+    description: "",
+    imageUrl: "",
+    isFavorite: false,
+    price: 0.0,
+  );
 
   @override
   void dispose() {
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageController.dispose();
     super.dispose();
+  }
+
+  void _saveForm() {
+    final _isValid = _form.currentState.validate();
+    if (_isValid) {
+      _form.currentState.save();
+    }
   }
 
   @override
@@ -22,26 +42,37 @@ class _EditProductPageState extends State<EditProductPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: _saveForm,
+          )
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Form(
+          key: _form,
           child: SingleChildScrollView(
             child: Column(
               children: <Widget>[
                 SizedBox(height: 4),
-                Container(
-                  height: 50,
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Title',
-                      border: OutlineInputBorder(),
-                    ),
-                    textInputAction: TextInputAction.next,
-                    onFieldSubmitted: (_) {
-                      FocusScope.of(context).requestFocus(_priceFocusNode);
-                    },
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please provide a value.';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Title',
+                    border: OutlineInputBorder(),
                   ),
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_priceFocusNode);
+                  },
+                  onSaved: (value) => _product.title = value,
                 ),
                 SizedBox(height: 8),
                 Container(
@@ -54,9 +85,11 @@ class _EditProductPageState extends State<EditProductPage> {
                     textInputAction: TextInputAction.next,
                     keyboardType: TextInputType.number,
                     focusNode: _priceFocusNode,
-                    onFieldSubmitted: (_){
-                      FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                    onFieldSubmitted: (_) {
+                      FocusScope.of(context)
+                          .requestFocus(_descriptionFocusNode);
                     },
+                    onSaved: (value) => _product.price = double.parse(value),
                   ),
                 ),
                 SizedBox(height: 8),
@@ -68,6 +101,45 @@ class _EditProductPageState extends State<EditProductPage> {
                   ),
                   keyboardType: TextInputType.multiline,
                   focusNode: _descriptionFocusNode,
+                  onSaved: (value) => _product.description = value,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: EdgeInsets.only(
+                        top: 8,
+                        right: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      child: _imageController.text.isEmpty
+                          ? Text('Enter an URL')
+                          : FittedBox(
+                              child: Image.network(_imageController.text),
+                              fit: BoxFit.cover,
+                            ),
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Image Url',
+                          border: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.done,
+                        controller: _imageController,
+                        onFieldSubmitted: (_) => _saveForm(),
+                        onSaved: (value) => _product.imageUrl = value,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
